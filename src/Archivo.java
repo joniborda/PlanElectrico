6974151a8bd2e531dee5998764e1f7c58c4d6989
joniborda <jonathanmatiasborda@gmail.com>
2018-11-22 22:39:48 -0300

    avaance
    avance

--- src/Archivo.java
+++ src/Archivo.java
@@ -1,4 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Archivo {
 
+	public PlanElectrico leer(String archivo) throws FileNotFoundException {
+		
+		File file = new File(archivo);
+		Scanner scanner = new Scanner(file);
+		
+		PlanElectrico planElectrico = new PlanElectrico(scanner.nextInt(), scanner.nextInt());
+		for (int i = 0; i < planElectrico.getCantidadCiudadesElectricas(); i++) {
+			planElectrico.addCiudadElectrica(scanner.nextInt());
+		}
+		
+		
+		for (int i = 0; i < planElectrico.getCantidadCiudades(); i++) {
+			for (int j = 0; j < planElectrico.getCantidadCiudades(); j++) {
+				
+			}
+		}
+		return null;
+	}
 }
---
+++ src/PlanElectrico.java
@@ -0,0 +1,24 @@
+
+public class PlanElectrico {
+
+	private int cantidadCiudades;
+	private int cantidadCiudadesElectricas;
+	
+	public PlanElectrico(int cantidadCiudades, int cantidadCiudadesElectricas) {
+		this.cantidadCiudades = cantidadCiudades;
+		this.cantidadCiudadesElectricas = cantidadCiudadesElectricas;
+	}
+
+	public int getCantidadCiudadesElectricas() {
+		return this.cantidadCiudadesElectricas;
+	}
+
+	public void addCiudadElectrica(int nextInt) {
+		
+	}
+
+	public int getCantidadCiudades() {
+		return this.cantidadCiudades;
+	}
+
+}
